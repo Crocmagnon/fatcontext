ef9d47d1f097531a5b5b31c8d77cdbab3325aed0
Gabriel Augendre <gabriel@augendre.info>
2025-01-13 15:46:00 +0100

    feat: better discriminate assignations to struct pointers

---
+++ pkg/analyzer/testdata/no_structpointer/example.go
@@ -0,0 +1,23 @@
+package common
+
+import (
+	"context"
+)
+
+type Container struct {
+	Ctx context.Context
+}
+
+func something() func(*Container) {
+	return func(r *Container) {
+		ctx := r.Ctx
+		ctx = context.WithValue(ctx, "key", "val")
+		r.Ctx = ctx
+	}
+}
+
+func blah(r *Container) {
+	ctx := r.Ctx
+	ctx = context.WithValue(ctx, "key", "val")
+	r.Ctx = ctx
+}
