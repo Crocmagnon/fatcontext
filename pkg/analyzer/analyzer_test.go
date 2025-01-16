7b0afb1f92ba41f734545b71c388f0744523f464
Fernandez Ludovic <ldez@users.noreply.github.com>
2025-01-17 00:37:25 +0100

    tests: rewrite tests

--- pkg/analyzer/analyzer_test.go
+++ pkg/analyzer/analyzer_test.go
@@ -1,8 +1,6 @@
 package analyzer_test
 
 import (
-	"os"
-	"path/filepath"
 	"testing"
 
 	"golang.org/x/tools/go/analysis/analysistest"
@@ -11,27 +9,49 @@ import (
 )
 
 func TestAnalyzer(t *testing.T) {
-	wd, err := os.Getwd()
-	if err != nil {
-		t.Fatalf("Failed to get wd: %s", err)
+	testCases := []struct {
+		desc    string
+		dir     string
+		options map[string]string
+	}{
+		{
+			desc: "no func decl",
+			dir:  "common",
+		},
+		{
+			desc: "no func decl",
+			dir:  "no_structpointer",
+		},
+		{
+			desc: "func decl",
+			dir:  "common",
+			options: map[string]string{
+				analyzer.FlagCheckStructPointers: "true",
+			},
+		},
+		{
+			desc: "func decl",
+			dir:  "structpointer",
+			options: map[string]string{
+				analyzer.FlagCheckStructPointers: "true",
+			},
+		},
 	}
-	testdata := filepath.Join(wd, "testdata")
 
-	t.Run("no func decl", func(t *testing.T) {
-		an := analyzer.NewAnalyzer()
-		analysistest.Run(t, testdata, an, "./common")
-		analysistest.Run(t, testdata, an, "./no_structpointer")
-	})
+	for _, test := range testCases {
+		t.Run(test.desc+"_"+test.dir, func(t *testing.T) {
+			t.Parallel()
 
-	t.Run("func decl", func(t *testing.T) {
-		an := analyzer.NewAnalyzer()
+			a := analyzer.NewAnalyzer()
 
-		err := an.Flags.Set(analyzer.FlagCheckStructPointers, "true")
-		if err != nil {
-			t.Fatal(err)
-		}
+			for k, v := range test.options {
+				err := a.Flags.Set(k, v)
+				if err != nil {
+					t.Fatal(err)
+				}
+			}
 
-		analysistest.Run(t, testdata, an, "./common")
-		analysistest.Run(t, testdata, an, "./structpointer")
-	})
+			analysistest.Run(t, analysistest.TestData(), a, test.dir)
+		})
+	}
 }
